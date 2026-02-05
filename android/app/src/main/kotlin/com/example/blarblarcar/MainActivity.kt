0833a7d3fea47b3a0b73a986358f09296f3a46b0
Visnuka <168633610+Sophavisnuka@users.noreply.github.com>
2026-02-05 11:19:19 +0700

    Implement filter for blarblar startcode

---
+++ android/app/src/main/kotlin/com/example/blarblarcar/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.blarblarcar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/services/rides_service.dart
@@ -0,0 +1,35 @@
+import '../data/dummy_data.dart';
+import '../model/ride/locations.dart';
+import '../model/ride/ride.dart';
+
+////
+///   This service handles:
+///   - The list of available rides
+///
+class RidesService {
+  static List<Ride> availableRides = fakeRides; // TODO for now fake data
+
+  static List<Ride> filterByDeparture(Location departure) {
+    return availableRides
+        .where((ride) => ride.departureLocation == departure)
+        .toList();
+  }
+
+  static List<Ride> filterBySeatRequested(int seatRequested) {
+    return availableRides
+        .where((ride) => ride.remainingSeats == seatRequested)
+        .toList();
+  }
+
+  static List<Ride> filterBy({Location? departure, int? seatRequested}) {
+    return availableRides.where((ride) {
+      if (departure != null && ride.departureLocation != departure) {
+        return false;
+      } else if (seatRequested != null &&
+          ride.remainingSeats != seatRequested) {
+        return false;
+      }
+      return true;
+    }).toList();
+  }
+}
